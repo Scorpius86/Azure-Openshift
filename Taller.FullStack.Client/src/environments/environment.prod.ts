0aa096b6cc88d36d15a7893156f4b8a975b06cd7
Erick Aróstegui Cunza <erick.arostegui.cunza@gmail.com>
2022-09-17 18:12:30 +0200

    Release 1.0.0.5

--- Taller.FullStack.Client/src/environments/environment.prod.ts
+++ Taller.FullStack.Client/src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   //apiServer: 'http://tallerfullstackservice-erick-arostegui-cunza-dev.apps.sandbox.x8i5.p1.openshiftapps.com'
   apiServer:'http://tallerfullstackservice.apps-crc.testing'
+  //apiServer:'http://localhost:8085'
 };
